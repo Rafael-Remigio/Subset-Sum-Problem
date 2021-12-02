ec6e319a4906803f227825bea99b9e72850b6323
Comrade-Remigio <rafael.lixa01@gmail.com>
2021-12-02 14:30:53 +0000

    BKLJASLKDJ
    ODEIO TE

--- test.c
+++ test.c
@@ -7,8 +7,8 @@
 #define n_sums      20
 #define n_problems  (max_n - min_n + 1)
 
-int Bf_Iter(int n, integer_t *p, integer_t *sums, integer_t desired_sum){
-    
+int Bf_Iter(int n, integer_t *p, integer_t desired_sum){
+
     int comb = 0;
     integer_t test_sum;
 
@@ -26,6 +26,45 @@ int Bf_Iter(int n, integer_t *p, integer_t *sums, integer_t desired_sum){
     return comb;
     
 }
+
+
+int Bf_recur(int n, integer_t *p, integer_t desired_sum,int i,int current_sum,int current_comb){
+    if (p[i] == p[n-1]) {
+        if (current_sum + 1 * p[i] == desired_sum){   
+            //change combination here
+            return current_comb;
+        }
+        return current_comb + 0 * p[i];
+    }
+
+    if (current_sum + 0 * p[i]  == desired_sum){
+        return current_comb;
+    }
+/*     else if (current_sum + 1 * p[i]  == desired_sum){
+        return current_comb * ;
+    } */
+
+}
+
+int print_all_sums_recursive(unsigned int n,unsigned int m,integer_t *p,double sum,unsigned long mask,integer_t desired_sum)
+{
+    if(m == n)
+    { // nothing more to do; print sum
+        unsigned int i,j;
+        if (sum == desired_sum){
+            printf("\n %lu \n",mask);
+            return mask;
+            
+        }
+    }
+    else
+    {
+        print_all_sums_recursive(n,m + 1u,p,sum ,mask ,desired_sum); // do not use a[m]
+        print_all_sums_recursive(n,m + 1u,p,sum + p[m],mask | (1ul << m),desired_sum); // use a[m]
+    }
+}
+
+
 
 char *Converter(int n,int x, char *sol){
     for(int bit=0; bit<n ;bit++){
@@ -58,8 +97,8 @@ int main(void)
     printf("\n");
      
 
-    // start looping for n's
-    for(int i = 0;i < 15;i++)
+   // start looping for n's
+   /*  for(int i = 0;i < 15;i++)
     {
         printf("--------------------------- \n");
         
@@ -82,7 +121,7 @@ int main(void)
 
             // run function and take time 
             double tmp_dt = cpu_time();   
-            int comb = Bf_Iter(n, p, sums, sum);
+            int comb = Bf_Iter(n, p, sum);
             tmp_dt = cpu_time() - tmp_dt;
             dt += tmp_dt;
 
@@ -93,8 +132,28 @@ int main(void)
         // store times 
         fprintf(fp,"%i %f \n",n, dt);
 
-    }
+    }   */
 
+     for(int i = 0;i < 15;i++)
+    {
+        printf("--------------------------- \n");
+ 
+        // get n
+        int n = all_subset_sum_problems[i].n;    
+        char comb_bin[n+1];
+        printf("n =  %i\n\n",n);
+        // get p and sums
+        integer_t *p = all_subset_sum_problems[i].p;    
+        integer_t *sums = all_subset_sum_problems[i].sums; 
+        
+        for(int j = 0;j < n_sums;j++){   
+            integer_t sum = all_subset_sum_problems[i].sums[j];
+            int combination = print_all_sums_recursive(n,0, p,0,0,sum);
+            printf("\nsoma = %llu --- solution = %s ", sum ,Converter(n, combination, comb_bin));
+
+        }
+
+    } 
     return 0;
 
     
